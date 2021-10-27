39154de67ac6d2fb2f9ad1431736123a022b59af
delmi <m0a2017192@edu.teu.ac.jp>
2021-10-27 18:56:39 +0900

    追加

--- WebClass_1027/function-test.js
+++ WebClass_1027/function-test.js
@@ -1,3 +1,21 @@
 function Sample1(){
   console.log("関数のテストです")
 }
+
+function Sample2(data1, data2, data3){
+  console.log(`data1: ${data1}`);
+  console.log(`data2: ${data2}`);
+  console.log(`data3: ${data3}`);
+}
+
+function Sample3(data1, data2 = "default"){
+  console.log(`data1: ${data1}`);
+  console.log(`data2: ${data2}`);
+}
+
+function Sample4(){
+  return 2021 * 1010;
+}
+
+let str = Sample4();
+  console.log(str);
