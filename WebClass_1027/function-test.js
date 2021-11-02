45eef6e53a78ac678ac46c4b6be51ce4d4e1b26b
delmi <m0a2017192@edu.teu.ac.jp>
2021-11-02 16:22:49 +0900

    追加

--- WebClass_1027/function-test.js
+++ WebClass_1027/function-test.js
@@ -17,5 +17,39 @@ function Sample4(){
   return 2021 * 1010;
 }
 
-let str = Sample4();
-  console.log(str);
+let smp = Sample4();
+  console.log(smp);
+
+
+//二つの引数の乗算
+
+function Multiply(data1, data2){
+  return data1 * data2;
+}
+
+let str = Multiply(123, 3);
+console.log(str);
+
+
+
+//二つの引数の乗算結果の1の位
+
+function Multiply2(data1, data2){
+  return data1 * data2;
+}
+
+let str2 = Multiply2(123, 3);
+let str2_ = Math.floor(str2 / 1) % 10;
+console.log(str2_);
+
+
+
+//二つの引数の乗算結果の10の位
+
+function Multiply3(data1, data2){
+  return data1 * data2;
+}
+
+let str3 = Multiply3(123, 3);
+let str3_ = Math.floor(str3 / 10) % 10;
+console.log(str3_);
