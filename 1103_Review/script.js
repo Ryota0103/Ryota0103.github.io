a4f2d4b2ec0c25d3ab9ca744cb6b983822abebe2
delmi <m0a2017192@edu.teu.ac.jp>
2021-11-09 10:52:45 +0900

    追加

---
+++ 1103_Review/script.js
@@ -0,0 +1,15 @@
+function hello(){
+  console.log("こんにちはこんにちは")
+}
+
+function helloSomeone(name){
+  console.log(`こんにちは${name}さん`)
+}
+
+function helloCircleArea(radius){
+  console.log((radius ** 2) * 3.14)
+}
+
+function helloRectangleArea(width, height){
+  console.log(width * height)
+}
---
+++ 1103_Web_2/modal.js
@@ -0,0 +1,5 @@
+function showModal(){
+  let a = document
+  modal.innerHTML = "ボタンがクリックされたぞ！"
+  modal.style.backgroundColor = "red";
+}
