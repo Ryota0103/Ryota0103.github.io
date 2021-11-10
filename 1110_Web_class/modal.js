9998644e4c250392f23a1fbb2046aa4c22132115
delmi <m0a2017192@edu.teu.ac.jp>
2021-11-10 18:26:34 +0900

    追加

--- 1110_Web_class/modal.js
+++ 1110_Web_class/modal.js
@@ -1,5 +1,5 @@
 function checkNumber(){
-  if(Number(document.querySelector('#num').value > 10){
+  if(Number(document.querySelector('#num').value) > 10){
     window.alert("true");
   }else{
     window.alert("false");
