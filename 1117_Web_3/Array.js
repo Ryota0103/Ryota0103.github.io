abf0db534515d4948789c7b47db009d702d60d1d
delmi <m0a2017192@edu.teu.ac.jp>
2021-11-24 14:52:38 +0900

    追加

--- 1117_Web_3/Array.js
+++ 1117_Web_3/Array.js
@@ -1,9 +1,17 @@
-function doCheckAnswers(){
+function doCheckAnswers() {
   let answer = document.querySelector("#answer");
-  let inputdata = document.querySelector("#input_string")
-  let modal = Array.from(document.querySelectorAll('.answer-list'));
+  let inputdata = document.querySelector("#input_string");
+  let modal = Array.from(document.querySelectorAll('ul.answer-list li'));
 
-  if(inputdata === modal{
-    answer.innerHTML = "正解";
+  modal.forEach(
+      function(element) {
+        if (inputdata.value === element.innerHTML) {
+          answer.innerHTML = "正解";
+        }
+      }
+   )
+  //配列のどの要素とも等しくなかった場合に不正解と表示
+  if(answer.innerHTML === ""){
+    answer.innerHTML = "不正解";
   }
 }
