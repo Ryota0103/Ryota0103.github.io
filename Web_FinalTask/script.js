cdc93368199e410b09bef5af359291149db9538b
delmi <m0a2017192@edu.teu.ac.jp>
2022-01-12 17:20:00 +0900

    追加

--- Web_FinalTask/script.js
+++ Web_FinalTask/script.js
@@ -1,20 +1,22 @@
 window.onload = function startAnimation(){
-  anime({
-    targets: '#animation-box-1',
-    translateX: 250,
-    opacity: 1,
-    scale: 1,
-    duration: 1500
-  })
-  anime({
-    targets: '#animation-box-2',
-    translateX: 250,
-    opacity: 1,
-    scale: 1,
-    duration: 1500,
-    delay:300
-  })
+    anime({
+      targets: '#animation-box-1',
+      translateX: 250,
+      opacity: 1,
+      scale: 1,
+      duration: 1500
+    })
+    anime({
+      targets: '#animation-box-2',
+      translateX: 250,
+      opacity: 1,
+      scale: 1,
+      duration: 1500,
+      delay: 300
+    })
 }
+
+
 
 document.getElementById("main-contents").style.display ="block";
 document.getElementById("works-contents").style.display ="none";
